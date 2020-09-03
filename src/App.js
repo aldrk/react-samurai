0dd12399f3720a37bf643c61ec090617dfc44455
Александр Круподеров <sashka.krup@gmail.com>
2020-09-03 23:18:29 +0300

    les7 done. What is component?

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Technoligies />
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">News</a>
+            <a href="#">Messages</a>
+        </div>
+    );
+}
+
+const Technoligies = () => {
+    return (
+        <div>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+                <li>ReactJS</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
