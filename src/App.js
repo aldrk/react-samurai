cda50d5c67b09838ac3ac93af925861fe22e5456
Александр Круподеров <sashka.krup@gmail.com>
2020-09-04 12:35:06 +0300

    les8-10 done. Export/Import

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import React from 'react';
+import Technologies from './Technologies';
+import Header from "./Header";
 import './App.css';
 
 const App = () => {
     return (
         <div>
             <Header />
-            <Technoligies />
+            <Technologies />
         </div>
     );
 }
 
-const Header = () => {
-    return (
-        <div>
-            <a href="#">Home</a>
-            <a href="#">News</a>
-            <a href="#">Messages</a>
-        </div>
-    );
-}
 
-const Technoligies = () => {
-    return (
-        <div>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JS</li>
-                <li>ReactJS</li>
-            </ul>
-        </div>
-    );
-}
+
+
 
 export default App;
