895b580c072347350a4b4d25904d4f372ab26163
Александр Круподеров <sashka.krup@gmail.com>
2020-09-04 16:47:33 +0300

    lesson 12 done.

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../App.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <header>
+                <img
+                    src="https://is5-ssl.mzstatic.com/image/thumb/Purple124/v4/b6/c3/cb/b6c3cb54-5fd6-3d1f-7b13-255aa6e56b3e/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png"
+                    alt="logo"/>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <nav>
+                <a href="#">Profile</a>
+                <a href="#">Messages</a>
+                <a href="#">News</a>
+                <a href="#">Music</a>
+                <a href="#">Settings</a>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
