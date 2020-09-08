d0bddcfde27202202fdbaaa6ea140352f77d5118
Александр Круподеров <sashka.krup@gmail.com>
2020-09-08 21:42:34 +0300

    lesson 24-25 done. Added BLL/UI, map for components

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+
 const DialogItem = (props) => {
     return (
         <div className={s.dialog + " " + s.active}>
-            <NavLink to={`/dialogs/1${props.id}`}>{props.name}</NavLink>
+            <NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
         </div>
     );
 }
@@ -18,20 +19,31 @@ const Message = (props) => {
     );
 }
 
+let dialogs = [
+    {id: 1, name: 'Dima'},
+    {id: 2, name: 'Mary'},
+    {id: 3, name: 'Anya'},
+    {id: 4, name: 'Liza'},
+    {id: 5, name: 'Igor'}
+]
+
+let messages = [
+    {id: 1, message: 'How do you do?'},
+    {id: 2, message: 'Fine, and you?'},
+    {id: 3, message: 'Me too'}
+]
+
+let dialogsComponents = dialogs.map( d => <DialogItem id={d.id} name={d.name}/>);
+let messageComponents = messages.map( m => <Message id={m.id} message={m.message}/>)
+
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem id="1" name='Dima' />
-                <DialogItem id="2" name='Mary' />
-                <DialogItem id="3" name='Anya' />
-                <DialogItem id="4" name='Liza' />
-                <DialogItem id="5" name='Igor' />
+                { dialogsComponents }
             </div>
             <div className={s.messages}>
-                <Message message='How do you do?'/>
-                <Message message='Fine, and you?'/>
-                <Message message='Me too'/>
+                { messageComponents }
             </div>
         </div>
     );
--- src/Components/Profle/MyPosts/MyPosts.jsx
+++ src/Components/Profle/MyPosts/MyPosts.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import Post from "./Post/Post";
 import s from './MyPosts.module.css'
 
+let posts = [
+    {id: 1, postMessage: 'Hi I`m learning react', likeCount: 5},
+    {id: 2, postMessage: 'Is it difficult?', likeCount: 1},
+]
+
+let postsComponents = posts.map(p => <Post postMessage={p.postMessage} likesCount={p.likeCount}/>)
+
 const MyPosts = () => {
     return (
         <div className={s.posts}>
@@ -15,8 +22,7 @@ const MyPosts = () => {
                     <button>Delete</button>
                 </div>
             </div>
-            <Post postMessage={'Hi I`m learning react'} likeCount={5}/>
-            <Post postMessage={'Is it difficult?'} likeCount={1}/>
+            {postsComponents}
         </div>
     );
 }
